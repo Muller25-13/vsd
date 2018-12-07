22df6b5d582404c0d650154bd1647d9f579c80b6
Zihao Fu <810640553@qq.com>
2018-12-07 09:17:40 +0800

    ddddd
    dddddssss

---
+++ WebApplication1/WebApplication1/Controllers/TestController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebApplication1.Models;
+
+namespace WebApplication1.Controllers
+{
+    public class TestController : Controller
+    {
+        // GET: Test
+       public string GetString()
+        {
+            return "hhh";
+        }
+
+        public Cust getCust()
+        {
+            Cust ct = new Cust();
+            ct.CustomerName = "fzh";
+            ct.Add = "gl";
+            return ct;
+        }
+    }
+}
---
+++ WebApplication1/WebApplication1/Models/Cust.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebApplication1.Models
+{
+    public class Cust
+    {
+        public string CustomerName
+        {
+            get;
+            set;
+        }
+        public string Add
+        {
+            get;
+            set;
+        }
+
+        public override string ToString()
+        {
+            return this.CustomerName + "-" + this.Add;
+        }
+    }
+}
